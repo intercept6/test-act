951f2ace75de9e3ea280974b614d83f6d4026747
intercept6 <intercept6@gmail.com>
2020-07-09 10:31:14 +0900

    CDKとActionsを作成する

--- bin/test-act.ts
+++ bin/test-act.ts
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { TestActStack } from '../lib/test-act-stack';
+import "source-map-support/register";
+import * as cdk from "@aws-cdk/core";
+import { TestActStack } from "../lib/test-act-stack";
+
+const account = process.env.AWS_ACCOUNT_ID!;
 
 const app = new cdk.App();
-new TestActStack(app, 'TestActStack');
+new TestActStack(app, "TestActStack", { env: { account } });
--- test/test-act.test.ts
+++ test/test-act.test.ts
@@ -1,13 +1,28 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
-import * as cdk from '@aws-cdk/core';
-import * as TestAct from '../lib/test-act-stack';
+import {
+  expect as expectCDK,
+  matchTemplate,
+  MatchStyle,
+} from "@aws-cdk/assert";
+import * as cdk from "@aws-cdk/core";
+import * as TestAct from "../lib/test-act-stack";
 
-test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new TestAct.TestActStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+test("Empty Stack", () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new TestAct.TestActStack(app, "MyTestStack");
+  // THEN
+  expectCDK(stack).to(
+    matchTemplate(
+      {
+        Resources: {
+          Bucket83908E77: {
+            Type: "AWS::S3::Bucket",
+            UpdateReplacePolicy: "Retain",
+            DeletionPolicy: "Retain",
+          },
+        },
+      },
+      MatchStyle.EXACT
+    )
+  );
 });
